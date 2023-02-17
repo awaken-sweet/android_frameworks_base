0c1e190b3087c221a4577a28a7d80a26316e858a
jhenrique09 <jhsv09@gmail.com>
2023-02-17 14:45:52 +0200

    Instrumentation: Get package name from context instead of app
    E AndroidRuntime: FATAL EXCEPTION: main
    E AndroidRuntime: Process: com.NextFloor.DestinyChild, PID: 11637
    E AndroidRuntime: java.lang.RuntimeException: Unable to get provider androidx.startup.InitializationProvider: androidx.startup.StartupException: android.content.pm.PackageManager$NameNotFoundException: ComponentInfo{/androidx.startup.InitializationProvider}
    E AndroidRuntime:   at android.app.ActivityThread.installProvider(ActivityThread.java:7488)
    
    idoybh: Adapted for us, no PPU change is needed
    Change-Id: I57c622af5628e109eb4c51bbd5260d78b171ea24

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1360,7 +1360,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PixelPropsUtils.setProps(app.getPackageName());
+        PixelPropsUtils.setProps(context.getPackageName());
         return app;
     }
     
@@ -1379,7 +1379,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PixelPropsUtils.setProps(app.getPackageName());
+        PixelPropsUtils.setProps(context.getPackageName());
         return app;
     }
 
