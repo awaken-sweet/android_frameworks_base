494cf61511e108452f62b47c73622d895fec9fa9
Abhay Singh Gill <abhaygill017@gmail.com>
2025-10-23 23:50:29 +0530

    KeyboxImitationHooks: Persist generated verified boot hash
    and some other fixes / improvements
    
    Change-Id: I9b69ffffa78dd31776b342a0fc2f337caca06b67
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13601,6 +13601,21 @@ public final class Settings {
         public static final String KEYBOX_DATA = "keybox_data";
 
         /**
+         * Store vboot key.
+         * @hide
+         */
+        @Readable
+        public static final String VBOOT_KEY = "vboot_key";
+
+
+        /**
+         * Store vboot hash.
+         * @hide
+         */
+        @Readable
+        public static final String VBOOT_HASH = "vboot_hash";
+
+        /**
          * Whether to use black theme for dark mode
          * @hide
          */
